7b28ce2018f35bd4a1b08f65ce8f01b558bfbe9e
Gabriel Dantas <gabriel.dnlc@gmail.com>
2023-01-12 15:55:33 -0300

    eliminating trashed folders from search + type hinting

--- utils/funcs.py
+++ utils/funcs.py
@@ -1,14 +1,16 @@
-def GetFolderList(drive, parent_id = 'root') -> list :
-    query = "'{}' in parents and mimeType='application/vnd.google-apps.folder'".format(parent_id)
+from pydrive2.drive import GoogleDrive
+
+def GetFolderList(drive : GoogleDrive, parent_id : str = 'root') -> list :
+    query = "'{}' in parents and mimeType='application/vnd.google-apps.folder' and trashed=false".format(parent_id)
     file_list = drive.ListFile({'q': query }).GetList()
     return file_list
 
-def PrintFolderList(drive, parent_id = 'root') -> None:
+def PrintFolderList(drive : GoogleDrive, parent_id : str = 'root') -> None:
     list = GetFolderList(drive, parent_id)
     for folder in list:
-        print('title: %s, id: %s' % (folder['title'], folder['id']))
+        print('Title: %s, id: %s' % (folder['title'], folder['id']))
 
-def CreateFolder(drive, folder_name, parent_id = 'root') -> bool:
+def CreateFolder(drive : GoogleDrive, folder_name : str, parent_id : str = 'root') -> bool:
     try:
         folder = drive.CreateFile({'title': folder_name, 
         "parents":  [{"id": parent_id}], 
@@ -18,13 +20,13 @@ def CreateFolder(drive, folder_name, parent_id = 'root') -> bool:
     except: # TODO extend error catching
         return False
 
-def FolderExists(drive, folder_name, parent_id = 'root') -> bool:
+def FolderExists(drive: GoogleDrive, folder_name : str, parent_id : str = 'root') -> bool | GoogleDrive: 
     list = GetFolderList(drive, parent_id)
 
-    found = False
     for folder in list:
         if (folder['title'] == folder_name):
-            found = True
+            return folder
             break
-    return found
+        
+    return False
 
