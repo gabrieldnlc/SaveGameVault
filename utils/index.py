28e2755e381b3c81e273c86fcb08905db05c1fec
Gabriel Dantas <gabriel.dnlc@gmail.com>
2023-01-18 17:19:36 -0300

    starting to index the subfolders

--- utils/index.py
+++ utils/index.py
@@ -4,18 +4,41 @@ from pydrive2.drive import GoogleDrive
 
 from .funcs import *
 
-class MainIndex:
+class MainIndex: 
+    # An object representing the Drive folders for the UI
     def __init__(self, drive : GoogleDrive):
-        self.update(drive)
+        self.games = {}
+        self.drive = drive
+        self.Update()
 
-    def update(self, drive : GoogleDrive, target_folder : GoogleDriveFile = None) -> bool :
+        
+    def PopulateIndexedGames(self, folder : GoogleDriveFile):
+        self.games = {}
+        folder_list = GetFolderList(self.drive, folder['id'])
+        for game in folder_list:
+            g = self.IndexedGame(self.drive, game)
+            self.games.append(g)
+            
+
+    def Update(self, target_folder : GoogleDriveFile = None) -> bool :
         if (target_folder == None):
-            target_folder = FolderExists(drive, vault_folder)
+            target_folder = FolderExists(self.drive, vault_folder)
             if (not target_folder):
-                target_folder = CreateFolder(drive, vault_folder)
-        games_list = GetFolderList(drive, target_folder['id'])
+                target_folder = CreateFolder(self.drive, vault_folder)
+        self.PopulateIndexedGames(target_folder)
         
 
+    class IndexedGame():
+        # An object representing one of the Save Game folders. MainIndex holds a list of these.
+        def __init__(self, drive : GoogleDrive, folder : GoogleDriveFile):
+            if (folder['mimeType'] != 'application/vnd.google-apps.folder'):
+                raise Exception("Cannot create an Index based on a single file.") # TODO: better exception
+            self.meta = {}
+            self.saves = {}
+            self.title = folder['title']
+            folders = GetFolderList(drive, folder['id'])
+
+        
     
         
     
