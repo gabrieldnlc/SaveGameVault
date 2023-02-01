85744d79af6e95ff0a407c6e806251cc5577c586
Gabriel Dantas <gabriel.dnlc@gmail.com>
2023-02-01 14:41:21 -0300

    adding properties(getters)

--- utils/file_management.py
+++ utils/file_management.py
@@ -3,29 +3,40 @@ from pathlib import Path
 class LocalFile():
 
     def __init__(self, path : str | Path):
-        try:
-            self.path = Path(path)
-            stat = self.path.stat()
+        self.path = Path(path)
 
-            if (self.path.is_dir()):
-                raise IsADirectoryError(f"'{path}' is not a file, but a folder.")
-            self.filename = self.path.name
-            self.size = stat.st_size
-            self.last_modified = stat.st_mtime
+        if (self.path.is_dir()):
+            raise IsADirectoryError(f"'{path}' is not a file, but a folder.")
 
-        except FileNotFoundError:
-            raise FileNotFoundError(f"'{path}' does not exist.") # defaulting to an English message instead of the OS language
-         
+        if (not self.path.exists()):
+            raise FileNotFoundError(f"'{path}' does not exist.")
+
+    @property
+    def stat(self):
+        return self.path.stat()
+    @property
+    def filename(self):
+        return self.path.name
+    @property
+    def size(self):
+        return self.stat.st_size
+    @property
+    def last_modified(self):
+        return self.stat.st_mtime
+
 class LocalFolder():
     def __init__(self, path : str | Path):
-        try:
-            self.path = Path(path)
-            stat = self.path.stat()
+        self.path = Path(path)
 
-            if (self.path.is_file()):
-                raise NotADirectoryError(f"'{path}' is not a folder, but a file.")
-            self.path = path
-            self.size = stat.st_size
+        if (self.path.is_file()):
+            raise NotADirectoryError(f"'{path}' is not a folder, but a file.")
 
-        except FileNotFoundError:
-            raise FileNotFoundError(f"'{path}' is not a valid folder.")   
+        if (not self.path.exists()):
+            raise FileNotFoundError(f"'{path} is not a valid folder.'")
+
+    @property
+    def stat(self):
+        return self.path.stat()
+    @property
+    def size(self):
+        return self.stat.st_size
